19977eb135c7a859d76fcb75f1f5eb515e2ebfa0
nikhiljainlive <nikhiljain5195@gmail.com>
2019-08-28 17:32:35 +0530

    trying with google login

---
+++ app/src/main/java/com/example/autobrowsersearch/JavaScripInterface.java
@@ -0,0 +1,16 @@
+package com.example.autobrowsersearch;
+
+import android.content.Context;
+
+//public class JavaScripInterface {
+//    private Context mContext;
+//    final String js = "javascript:document.getElementsByClassName" +
+//            "('whsOnd zHQkBf')[0].value='{your email}'; document.getElementById('identifierNext')" +
+//            ".click()";
+//
+//    public JavaScripInterface(Context context) {
+//        this.mContext = context;
+//    }
+//
+//    public void
+//}
--- app/src/main/java/com/example/autobrowsersearch/MainActivity.java
+++ app/src/main/java/com/example/autobrowsersearch/MainActivity.java
@@ -1,25 +1,28 @@
 package com.example.autobrowsersearch;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.webkit.ValueCallback;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-
-//    private Context mContext;
-//    private Activity mActivity;
-
-    private LinearLayout mRootLayout;
+    // The target url to surf using web view
+    private static final String TAG = "MainActivity";
+    private static String SEARCH_URL = "https://accounts.google.com/signin";
     private WebView mWebView;
+    private SwipeRefreshLayout refreshLayout;
 
-
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,35 +32,40 @@ public class MainActivity extends AppCompatActivity {
 //        mContext = getApplicationContext();
 //        mActivity = MainActivity.this;
 
-        // Get the widget reference from xml layout
-        mRootLayout = findViewById(R.id.root_layout);
-        mWebView = findViewById(R.id.web_view);
+        // findViews
+        findViews();
+        setupWebView();
+        setupClickListeners();
+    }
 
-        // The target url to surf using web view
-        String url = "https://accounts.google.com/signin";
+    private void setupClickListeners() {
+        refreshLayout.setOnRefreshListener(this::refreshWebView);
+    }
 
+    private void refreshWebView() {
+        mWebView.reload();
+        refreshLayout.setRefreshing(false);
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private void setupWebView() {
         /*
-            WebView
-                A View that displays web pages. This class is the basis upon which you can roll
-                your own web browser or simply display some online content within your Activity.
-                It uses the WebKit rendering engine to display web pages and includes methods
-                to navigate forward and backward through a history, zoom in and out,
-                perform text searches and more.
+        WebView:
+        A View that displays web pages. This class is the basis upon which you can roll
+        your own web browser or simply display some online content within your Activity.
+        It uses the WebKit rendering engine to display web pages and includes methods
+        to navigate forward and backward through a history, zoom in and out,
+        perform text searches and more.
         */
+
         // Load the url in web view
-        mWebView.loadUrl(url);
+        mWebView.loadUrl(SEARCH_URL);
 
         // Enable java script on web view
         mWebView.getSettings().setJavaScriptEnabled(true);
 
-        // The java script string to execute in web view after page loaded
-        // First line put a value in input box
-        // Second line submit the form
-        final String js = "javascript:document.getElementsByClassName" +
-                "('whsOnd zHQkBf')[0].value='{your email}'; document.getElementById('identifierNext').click()";
-
         // Set a web view client for web view
-        mWebView.setWebViewClient(new WebViewClient(){
+        mWebView.setWebViewClient(new WebViewClient() {
             /*
                 void onPageFinished (WebView view, String url)
                     Notify the host application that a page has finished loading. This method is
@@ -69,15 +77,57 @@ public class MainActivity extends AppCompatActivity {
                     view WebView : The WebView that is initiating the callback.
                     url String : The url of the page.
             */
-            public void onPageFinished(WebView view, String url){
-                if(Build.VERSION.SDK_INT >= 19){
-                    view.evaluateJavascript(js, new ValueCallback<String>() {
-                        @Override
-                        public void onReceiveValue(String s) {
-                        }
-                    });
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                Log.e(TAG, "onPageFinished: url loaded - " + url);
+
+                if (url.equalsIgnoreCase(SEARCH_URL)) {
+                    // The java script string to execute in web view after page loaded
+                    // First line put a value in input box
+                    // Second line submit the form
+                    final String js = "javascript:document.getElementsByClassName" +
+                            "('whsOnd zHQkBf')[0].value='nikhiljain5195@gmail.com'; " +
+                            "document.getElementById('identifierNext').click()";
+                    view.loadUrl(js);
+//                    view.evaluateJavascript(js, new ValueCallback<String>() {
+//                        @Override
+//                        public void onReceiveValue(String str) {
+//                            Log.e(TAG, "onReceiveValue: " + str);
+//                        }
+//                    });
                 }
+                if (url.equalsIgnoreCase("https://accounts.google.com/signin")) {
+                    // The java script string to execute in web view after page loaded
+                    // First line put a value in input box
+                    // Second line submit the form
+                    final String js1 = "javascript:document.getElementsByClassName" +
+                            "('whsOnd zHQkBf')[0].value='8871152221'; " +
+                            "document.getElementById('passwordNext').click()";
+                    view.loadUrl(js1);
+//                    view.evaluateJavascript(js, new ValueCallback<String>() {
+//                        @Override
+//                        public void onReceiveValue(String str) {
+//                            Log.e(TAG, "onReceiveValue: " + str);
+//                        }
+//                    });
+                }
+
             }
         });
+    }
+
+    private void findViews() {
+        mWebView = findViewById(R.id.web_view);
+        refreshLayout = findViewById(R.id.swipe_refresh_layout);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
